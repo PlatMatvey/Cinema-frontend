d0153bd9a1a802a026f8c3ae2d3f98ef38176512
PlatMatvey <matvey0910008@gmail.com>
2025-05-20 22:10:13 +0600

    3

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home, list_movie, list_serials
+from . import views
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('movies/', list_movie),
     path('serials/', list_serials),
+    path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail')
 ]
--- movie/views.py
+++ movie/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 import requests
 # Create your views here.
 def home(request):
-    response = requests.get("http://127.0.0.1:8080/advertising/carrousel")
-    carrousel = response.json()
-    print(carrousel)
-    return render(request, 'movie/master.html',{"carrousel": carrousel})
+    response_movies = requests.get("http://127.0.0.1:8080/movies/movie")
+    movie_list = response_movies.json()
+    response_carrousel = requests.get("http://127.0.0.1:8080/advertising/carrousel")
+    carrousel = response_carrousel.json()
+    response = requests.get("http://127.0.0.1:8080/movies/serials")
+    serial_list = response.json()
+    return render(request, 'movie/master.html', {
+        "movie_list": movie_list,
+        "carrousel": carrousel,
+        "serial_list": serial_list
+    })
 
 def list_movie(request):
     response = requests.get("http://127.0.0.1:8080/movies/movie")
@@ -18,3 +25,8 @@ def list_serials(request):
     serial_list = response.json()
     print(serial_list)
     return render(request, 'movie/list_serial.html', {"serial_list": serial_list})
+
+def movie_detail(request, movie_id):
+    response = requests.get(f"http://127.0.0.1:8080/movies/movie/{movie_id}")
+    movie = response.json()
+    return render(request, 'movie/deteil_movie.html', {'movie': movie})
