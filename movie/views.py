22e4922f21d6f1dadc51fa53ab9974be31ee81e0
PlatMatvey <matvey0910008@gmail.com>
2025-05-28 17:47:33 +0600

    10

--- movie/views.py
+++ movie/views.py
@@ -22,7 +22,7 @@ def list_movie(request):
 
 def list_serials(request):
     response = requests.get("http://127.0.0.1:8080/movies/serials")
-    serial_list = response.json()
+    serial_list = response.json()[:4]
     print(serial_list)
     return render(request, 'movie/list_serials.html', {"serial_list": serial_list})
 
@@ -36,38 +36,58 @@ def serial_detail(request, serial_id):
     serial = response.json()
     return render(request, 'movie/detail_serial.html', {'serial': serial})
 
+
 def all_movie(request):
     response = requests.get("http://127.0.0.1:8080/movies/movie")
     movie_all = response.json()
-    selected_genre = request.GET.getlist('genre')
-    selected_format = request.GET.getlist('format')
-    def movie_filter(n):
-        if selected_genre and n.get('genre') not in selected_genre:
-            return False
-        if selected_format and n.get('format') not in selected_format:
-            return False
-        return True
-    filtered = [n for n in movie_all if movie_filter(n)]
+    genres_set = set()
+    for m in movie_all:
+        raw = m.get('genre', '')
+        for g in raw.split(','):
+            g = g.strip()
+            if g:
+                genres_set.add(g)
+    all_genres = sorted(genres_set)
+    selected_genres = request.GET.getlist('genre')
+    selected_formats = request.GET.getlist('format')
+    if selected_genres:
+        def has_genre(m):
+            parts = [x.strip() for x in m.get('genre', '').split(',')]
+            return any(g in parts for g in selected_genres)
+        movie_all = [m for m in movie_all if has_genre(m)]
+    if selected_formats:
+        movie_all = [m for m in movie_all if m.get('format') in selected_formats]
     return render(request, 'movie/all_movie.html', {
-        'movie_all': filtered,
-        'selected_genre': selected_genre,
-        'selected_format': selected_format,
+        'movie_all': movie_all,
+        'all_genres': all_genres,
+        'selected_genres': selected_genres,
+        'selected_formats': selected_formats,
     })
 
 def all_serials(request):
     response = requests.get("http://127.0.0.1:8080/movies/serials")
     serial_all = response.json()
-    selected_genre = request.GET.getlist('genre')
-    selected_format = request.GET.getlist('format')
-    def serial_filter(n):
-        if selected_genre and n.get('genre') not in selected_genre:
-            return False
-        if selected_format and n.get('format') not in selected_format:
-            return False
-        return True
-    filtered = [n for n in serial_all if serial_filter(n)]
+    genres_set = set()
+    for m in serial_all:
+        raw = m.get('genre', '')
+        for g in raw.split(','):
+            g = g.strip()
+            if g:
+                genres_set.add(g)
+    all_genres = sorted(genres_set)
+    selected_genres = request.GET.getlist('genre')
+    selected_formats = request.GET.getlist('format')
+    if selected_genres:
+        def has_genre(m):
+            parts = [x.strip() for x in m.get('genre', '').split(',')]
+            return any(g in parts for g in selected_genres)
+
+        serial_all = [m for m in serial_all if has_genre(m)]
+    if selected_formats:
+        serial_all = [m for m in serial_all if m.get('format') in selected_formats]
     return render(request, 'movie/all_serial.html', {
-        'serial_all': filtered,
-        'selected_genre': selected_genre,
-        'selected_format': selected_format,
+        'serial_all': serial_all,
+        'all_genres': all_genres,
+        'selected_genres': selected_genres,
+        'selected_formats': selected_formats,
     })
