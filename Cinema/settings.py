8f61d9b3f8a1fbd8dd139aef8c5190bcade416c2
PlatMatvey <matvey0910008@gmail.com>
2025-06-02 17:53:53 +0600

    10

--- Cinema/settings.py
+++ Cinema/settings.py
@@ -124,6 +124,7 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+X_FRAME_OPTIONS = 'ALLOWALL'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
